782dd8bfeebd372d216b63fcd4fab8d844fd8939
Dipangkar H <idipangkar02@gmail.com>
2026-09-04 12:05:52 +0000

    feat: add Frappe Docker PP DocType

---
+++ my_custom_app/my_custom_app/doctype/frappe_docker_pp/frappe_docker_pp.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Dipangkar Hajong and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Frappe Docker PP", {
+// 	refresh(frm) {
+
+// 	},
+// });
